5697d023e01edf9967c42053b8eaa3cad072c6f8
julien-leduc <33868750+julien-leduc@users.noreply.github.com>
2017-12-08 10:32:11 +0100

    Update calc.js

--- src/calc.js
+++ src/calc.js
@@ -1,16 +1,36 @@
-/*
-** signe: '*', '+', '-', '/'
-** gauche: un nombre non negatif
-** droite: un nombre plus petit que 430
-**
-** La fonction renvoie le resultat du calcul
-**
-** La fonction renvoie null en cas d'erreur
-*/
 function calculatrice(signe, gauche, droite) {
-    var result = null;
-
-    return result;
+  var result;
+    if (typeof gauche === 'number' && typeof droite === 'number') {
+	   	if (gauche>=0 && droite<430 && droite!=0) {
+			switch (signe) {
+				case "*":
+					result = gauche*droite;
+					return result;
+					break;
+				case "+":
+					result = gauche+droite;
+					return result;
+					break;
+				case "-":
+					result = gauche-droite;
+					return result;
+					break;
+				case "/":
+					result = gauche/droite;
+					return result;
+					break;
+				default:
+					result=null;
+					return result;
+			} 
+		} else {
+		   	result = null;
+		   	return result;
+		}
+	}else{
+		result=null;
+		return result;
+	}
 }
  
 module.exports = calculatrice;
